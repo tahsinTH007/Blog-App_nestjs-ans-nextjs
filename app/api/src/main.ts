d9aff0414f1c872f942f9a1a8b42607c963d51ca
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-05-13 21:41:30 +0600

    cls

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
